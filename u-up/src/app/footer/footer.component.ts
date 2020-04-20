9d4ccbe7581d83a2580b00c54ffa093e2327e8d1
Gabriel Lima <nickspoot@gmail.com>
2020-04-20 19:56:48 -0300

    created uup spa angular projectnavbar, footer and homepage. #wb

--- u-up/src/app/footer/footer.component.ts
+++ u-up/src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 
   constructor() { }
 
